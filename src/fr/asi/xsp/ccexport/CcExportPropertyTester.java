aed8c54642d3dffe21e7f40e635ffab9261241e6
Lionel HERVIER <lhervier@asi-informatique.fr>
2014-09-17 23:35:10 +0200

    Le menu "Cc Export" n'apparait que sur les projets Domino

--- src/fr/asi/xsp/ccexport/CcExportPropertyTester.java
+++ src/fr/asi/xsp/ccexport/CcExportPropertyTester.java
@@ -2,22 +2,33 @@ package fr.asi.xsp.ccexport;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
 import com.ibm.designer.domino.ide.resources.DominoResourcesPlugin;
 
-public class DominoProjectPropertyTester extends PropertyTester {
+/**
+ * Testeur de propriétés pour activer/masquer les menus
+ * @author Lionel HERVIER
+ */
+public class CcExportPropertyTester extends PropertyTester {
 
-	public final static String PROPERTY_NAME = "isDominoProject";
+	/**
+	 * Pour savoir si on est sur un projet Domino
+	 */
+	public final static String PROP_ISDOMINOPROJECT = "isDominoProject";
+	
+	/**
+	 * Pour savoir si la synchro est en place
+	 */
+	public final static String PROP_ISSETUP = "isSetup";
 	
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
 		if (property == null)
 			return false;
 		if( !(receiver instanceof IStructuredSelection) )
-			return false;
-		if( !property.equals(PROPERTY_NAME) )
 			return false;
 		
 		IStructuredSelection selection = (IStructuredSelection) receiver;
@@ -30,6 +41,16 @@ public class DominoProjectPropertyTester extends PropertyTester {
 		IAdaptable adaptable = (IAdaptable) obj;
 		
 		IProject prj = (IProject) adaptable.getAdapter(IProject.class);
-		return DominoResourcesPlugin.isDominoDesignerProject(prj);
+		
+		if( PROP_ISDOMINOPROJECT.equals(property) ) {
+			return DominoResourcesPlugin.isDominoDesignerProject(prj);
+		} else if( PROP_ISSETUP.equals(property) ) {
+			try {
+				return null != prj.getPersistentProperty(Utils.PROP_PROJECT_NAME);
+			} catch (CoreException e) {
+				return false;
+			}
+		} else
+			return false;
 	}
 }
